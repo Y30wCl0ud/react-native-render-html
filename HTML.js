f58e442600fe6281777917b4d6af31c1ab86dc63
Kangyun Wang <wky@Kangyuns-MacBook-Pro.local>
2017-07-18 15:37:35 +0200

    Change to div

--- HTML.js
+++ HTML.js
@@ -182,7 +182,7 @@ export default class HTML extends React.Component {
 					return false;
 				}
 
-				if (node.name === 'span') {
+				if (node.name === 'div') {
 					if (node.attribs['data-video-id']) {
 						ElementsToRender = (
 							<Youtube
