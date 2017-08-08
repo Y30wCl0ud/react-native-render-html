691f5ec323583edbec9e17ec3eed4d0bcca5ef16
Kangyun Wang <wky@Kangyuns-MacBook-Pro.local>
2017-08-08 09:56:25 +0200

    Removed potential attributes (style & class) to prevent crashes

--- HTMLElement.js
+++ HTMLElement.js
@@ -4,115 +4,120 @@ import shallowCompare from 'react-addons-shallow-compare';
 import HTMLStyles from './HTMLStyles';
 
 class HTMLElement extends React.Component {
-  /* ****************************************************************************/
-  // Class
-  /* ****************************************************************************/
+	/* ****************************************************************************/
+	// Class
+	/* ****************************************************************************/
 
-    static propTypes = {
-        tagName: React.PropTypes.string.isRequired,
-        renderers: React.PropTypes.object.isRequired,
-        groupInfo: React.PropTypes.object,
-        parentTagName: React.PropTypes.string,
-        htmlStyles: React.PropTypes.object,
-        htmlAttibs: React.PropTypes.object,
-        onLinkPress: React.PropTypes.func,
-        children: React.PropTypes.node,
-        parentIsText: React.PropTypes.bool.isRequired
-    }
+	static propTypes = {
+		tagName: React.PropTypes.string.isRequired,
+		renderers: React.PropTypes.object.isRequired,
+		groupInfo: React.PropTypes.object,
+		parentTagName: React.PropTypes.string,
+		htmlStyles: React.PropTypes.object,
+		htmlAttibs: React.PropTypes.object,
+		onLinkPress: React.PropTypes.func,
+		children: React.PropTypes.node,
+		parentIsText: React.PropTypes.bool.isRequired
+	}
 
-  /* ****************************************************************************/
-  // Data Lifecycle
-  /* ****************************************************************************/
+	/* ****************************************************************************/
+	// Data Lifecycle
+	/* ****************************************************************************/
 
-    shouldComponentUpdate (nextProps, nextState) {
-        return shallowCompare(this, nextProps, nextState);
-    }
+	shouldComponentUpdate(nextProps, nextState) {
+		return shallowCompare(this, nextProps, nextState);
+	}
 
-  /* ****************************************************************************/
-  // UI Events
-  /* ****************************************************************************/
+	/* ****************************************************************************/
+	// UI Events
+	/* ****************************************************************************/
 
-    handleLinkPressed (evt) {
-        if (this.props.onLinkPress) {
-            this.props.onLinkPress(evt, this.props.onLinkPressArg);
-        }
-    }
+	handleLinkPressed(evt) {
+		if (this.props.onLinkPress) {
+			this.props.onLinkPress(evt, this.props.onLinkPressArg);
+		}
+	}
 
-  /* ****************************************************************************/
-  // Rendering
-  /* ****************************************************************************/
+	/* ****************************************************************************/
+	// Rendering
+	/* ****************************************************************************/
 
-  /**
-  * Generates the prefix nodes
-  * @return prefix nodes if applicable
-  */
-    prefixNode () {
-        if (this.props.tagName === 'li') {
-            if (this.props.parentTagName === 'ol') {
-                return <Text>{`\n${this.props.groupInfo.index + 1}). `}</Text>;
-            } else {
-                return <Text>{"\n• "}</Text>;
-            }
-        } else if (this.props.tagName === 'br') {
-            return <Text>{'\n'}</Text>;
-        } else {
-            return undefined;
-        }
-    }
+	/**
+	* Generates the prefix nodes
+	* @return prefix nodes if applicable
+	*/
+	prefixNode() {
+		if (this.props.tagName === 'li') {
+			if (this.props.parentTagName === 'ol') {
+				return <Text>{`\n${this.props.groupInfo.index + 1}). `}</Text>;
+			} else {
+				return <Text>{"\n• "}</Text>;
+			}
+		} else if (this.props.tagName === 'br') {
+			return <Text>{'\n'}</Text>;
+		} else {
+			return undefined;
+		}
+	}
 
-  /**
-  * @return the class for this node
-  */
-    elementClass () {
-        if (HTMLStyles.blockElements.has(this.props.tagName)) {
-            if (this.props.parentIsText) {
-                console.warn([
-                    'You are trying to nest a non-text HTML element inside a text element.',
-                    'The following nodes can only be rendered within themselves and not within text nodes:'
-                ].concat(Array.from(HTMLStyles.blockElements)).join('\n'));
-                return Text;
-            } else {
-                return View;
-            }
-        } else {
-            return Text;
-        }
-    }
+	/**
+	* @return the class for this node
+	*/
+	elementClass() {
+		if (HTMLStyles.blockElements.has(this.props.tagName)) {
+			if (this.props.parentIsText) {
+				console.warn([
+					'You are trying to nest a non-text HTML element inside a text element.',
+					'The following nodes can only be rendered within themselves and not within text nodes:'
+				].concat(Array.from(HTMLStyles.blockElements)).join('\n'));
+				return Text;
+			} else {
+				return View;
+			}
+		} else {
+			return Text;
+		}
+	}
 
-    render () {
-        const { htmlStyles, tagName, htmlAttribs, renderers, children, imagesMaxWidth, ...passProps } = this.props;
+	render() {
+		const { htmlStyles, tagName, htmlAttribs, renderers, children, imagesMaxWidth, ...passProps } = this.props;
 
-        if (renderers[tagName]) {
-            const copyProps = [
-                'htmlStyles',
-                'groupInfo',
-                'parentTagName',
-                'onLinkPress',
-                'parentIsText',
-                'imagesMaxWidth'
-            ].reduce((acc, k) => {
-                acc[k] = this.props[k];
-                return acc;
-            }, {});
-            return renderers[tagName](htmlAttribs, children, copyProps);
-        } else {
-            const RNElem = this.elementClass();
-            const styleset = RNElem === Text ? HTMLStyles.STYLESETS.TEXT : HTMLStyles.STYLESETS.VIEW;
-            const style = []
-        .concat(
-          HTMLStyles.defaultStyles[tagName],
-          htmlStyles ? htmlStyles[tagName] : undefined,
-          htmlAttribs.style ? HTMLStyles.cssStringToRNStyle(htmlAttribs.style, styleset) : undefined
-        ).filter((s) => s !== undefined);
+		if (htmlAttribs.style) {
+			delete htmlAttribs.style;
+			delete htmlAttribs.class;
+		}
 
-            return (
-        <RNElem {...passProps} style={style}>
-          {this.prefixNode()}
-          {this.props.children}
-        </RNElem>
-      );
-        }
-    }
+		if (renderers[tagName]) {
+			const copyProps = [
+				'htmlStyles',
+				'groupInfo',
+				'parentTagName',
+				'onLinkPress',
+				'parentIsText',
+				'imagesMaxWidth'
+			].reduce((acc, k) => {
+				acc[k] = this.props[k];
+				return acc;
+			}, {});
+			return renderers[tagName](htmlAttribs, children, copyProps);
+		} else {
+			const RNElem = this.elementClass();
+			const styleset = RNElem === Text ? HTMLStyles.STYLESETS.TEXT : HTMLStyles.STYLESETS.VIEW;
+			const style = []
+				.concat(
+				HTMLStyles.defaultStyles[tagName],
+				htmlStyles ? htmlStyles[tagName] : undefined,
+				htmlAttribs.style ? HTMLStyles.cssStringToRNStyle(htmlAttribs.style, styleset) : undefined
+				).filter((s) => s !== undefined);
+
+			return (
+				<RNElem {...passProps} style={style}>
+					{this.prefixNode()}
+					{this.props.children}
+				</RNElem>
+			);
+		}
+	}
 }
 
 module.exports = HTMLElement;
--- HTMLStyles.js
+++ HTMLStyles.js
@@ -7,16 +7,16 @@ import _RNViewStylePropTypes from 'react-native/Libraries/Components/View/ViewSt
 import _RNImageStylePropTypes from 'react-native/Libraries/Image/ImageStylePropTypes';
 
 const RNTextStylePropTypes = Object.keys(_RNTextStylePropTypes)
-  .reduce((acc, k) => { acc[k] = _RNTextStylePropTypes[k]; return acc; }, {});
+	.reduce((acc, k) => { acc[k] = _RNTextStylePropTypes[k]; return acc; }, {});
 const RNViewStylePropTypes = Object.keys(_RNViewStylePropTypes)
-  .reduce((acc, k) => { acc[k] = _RNViewStylePropTypes[k]; return acc; }, {});
+	.reduce((acc, k) => { acc[k] = _RNViewStylePropTypes[k]; return acc; }, {});
 const RNImageStylePropTypes = Object.keys(_RNImageStylePropTypes)
-  .reduce((acc, k) => { acc[k] = _RNImageStylePropTypes[k]; return acc; }, {});
+	.reduce((acc, k) => { acc[k] = _RNImageStylePropTypes[k]; return acc; }, {});
 
 const STYLESETS = Object.freeze({
-    VIEW: 'view',
-    TEXT: 'text',
-    IMAGE: 'image'
+	VIEW: 'view',
+	TEXT: 'text',
+	IMAGE: 'image'
 });
 
 const stylePropTypes = {};
@@ -26,181 +26,181 @@ stylePropTypes[STYLESETS.IMAGE] = Object.assign({}, RNViewStylePropTypes, RNImag
 
 class HTMLStyles {
 
-  /* ****************************************************************************/
-  // Lifecycle
-  /* ****************************************************************************/
+	/* ****************************************************************************/
+	// Lifecycle
+	/* ****************************************************************************/
 
-    constructor () {
-        this.defaultStyles = this._generateDefaultStyles();
+	constructor() {
+		this.defaultStyles = this._generateDefaultStyles();
 
-        // RN doesn't support css 'display:inline|block' so we have to treat these differently
-        this.blockElements = [
-            'div', 'ol', 'ul'
-        ].reduce((acc, n) => { acc.add(n); return acc; }, new Set());
-    }
+		// RN doesn't support css 'display:inline|block' so we have to treat these differently
+		this.blockElements = [
+			'div', 'ol', 'ul'
+		].reduce((acc, n) => { acc.add(n); return acc; }, new Set());
+	}
 
-  /* ****************************************************************************/
-  // Properties
-  /* ****************************************************************************/
+	/* ****************************************************************************/
+	// Properties
+	/* ****************************************************************************/
 
-    get STYLESETS () { return STYLESETS; }
+	get STYLESETS() { return STYLESETS; }
 
-  /* ****************************************************************************/
-  // Generating
-  /* ****************************************************************************/
+	/* ****************************************************************************/
+	// Generating
+	/* ****************************************************************************/
 
-  /**
-  * Small utility for generating heading styles
-  * @param baseFontSize: the basic font size
-  * @param fontMultiplier: the amount to multiply the font size by
-  * @param marginMultiplier: the amount to multiply the margin by
-  * @return a style def for a heading
-  */
-    _generateHeadingStyle (baseFontSize, fontMultiplier, marginMultiplier) {
-        return {
-            fontSize: baseFontSize * fontMultiplier,
-            marginTop: (baseFontSize * fontMultiplier) * marginMultiplier,
-            marginBottom: (baseFontSize * fontMultiplier) * marginMultiplier,
-            fontWeight: 'bold'
-        };
-    }
+	/**
+	* Small utility for generating heading styles
+	* @param baseFontSize: the basic font size
+	* @param fontMultiplier: the amount to multiply the font size by
+	* @param marginMultiplier: the amount to multiply the margin by
+	* @return a style def for a heading
+	*/
+	_generateHeadingStyle(baseFontSize, fontMultiplier, marginMultiplier) {
+		return {
+			fontSize: baseFontSize * fontMultiplier,
+			marginTop: (baseFontSize * fontMultiplier) * marginMultiplier,
+			marginBottom: (baseFontSize * fontMultiplier) * marginMultiplier,
+			fontWeight: 'bold'
+		};
+	}
 
-  /**
-  * Generates the default styles
-  * @return the stylesheet
-  */
-    _generateDefaultStyles () {
-    // These styles are mainly adapted from
-    // https://chromium.googlesource.com/chromium/blink/+/master/Source/core/css/html.css
+	/**
+	* Generates the default styles
+	* @return the stylesheet
+	*/
+	_generateDefaultStyles() {
+		// These styles are mainly adapted from
+		// https://chromium.googlesource.com/chromium/blink/+/master/Source/core/css/html.css
 
-        const BASE_FONT_SIZE = 14;
-        return StyleSheet.create({
-            // Block level elements
-            div: { },
+		const BASE_FONT_SIZE = 14;
+		return StyleSheet.create({
+			// Block level elements
+			div: {},
 
-            // Typography
-            p: {
-                fontSize: BASE_FONT_SIZE,
-                marginTop: BASE_FONT_SIZE,
-                marginBottom: BASE_FONT_SIZE
-            },
-            u: { textDecorationLine: 'underline' },
-            em: { fontStyle: 'italic' },
-            b: { fontWeight: 'bold' },
-            strong: { fontWeight: 'bold' },
-            big: { fontSize: BASE_FONT_SIZE * 1.2 },
-            small: { fontSize: BASE_FONT_SIZE * 0.8 },
-            a: {
-                textDecorationLine: 'underline',
-                color: '#245dc1'
-            },
+			// Typography
+			p: {
+				fontSize: BASE_FONT_SIZE,
+				marginTop: BASE_FONT_SIZE,
+				marginBottom: BASE_FONT_SIZE
+			},
+			u: { textDecorationLine: 'underline' },
+			em: { fontStyle: 'italic' },
+			b: { fontWeight: 'bold' },
+			strong: { fontWeight: 'bold' },
+			big: { fontSize: BASE_FONT_SIZE * 1.2 },
+			small: { fontSize: BASE_FONT_SIZE * 0.8 },
+			a: {
+				textDecorationLine: 'underline',
+				color: '#245dc1'
+			},
 
-            // Typography : Headers
-            h1: this._generateHeadingStyle(BASE_FONT_SIZE, 2, 0.67),
-            h2: this._generateHeadingStyle(BASE_FONT_SIZE, 1.5, 0.83),
-            h3: this._generateHeadingStyle(BASE_FONT_SIZE, 1.17, 1),
-            h4: this._generateHeadingStyle(BASE_FONT_SIZE, 1, 1.33),
-            h5: this._generateHeadingStyle(BASE_FONT_SIZE, 0.83, 1.67),
-            h6: this._generateHeadingStyle(BASE_FONT_SIZE, 0.67, 2.33),
+			// Typography : Headers
+			h1: this._generateHeadingStyle(BASE_FONT_SIZE, 2, 0.67),
+			h2: this._generateHeadingStyle(BASE_FONT_SIZE, 1.5, 0.83),
+			h3: this._generateHeadingStyle(BASE_FONT_SIZE, 1.17, 1),
+			h4: this._generateHeadingStyle(BASE_FONT_SIZE, 1, 1.33),
+			h5: this._generateHeadingStyle(BASE_FONT_SIZE, 0.83, 1.67),
+			h6: this._generateHeadingStyle(BASE_FONT_SIZE, 0.67, 2.33),
 
-            // Typography : Lists
-            ul: {
-                paddingLeft: 40,
-                marginBottom: BASE_FONT_SIZE
-            },
-            ol: {
-                paddingLeft: 40,
-                marginBottom: BASE_FONT_SIZE
-            },
+			// Typography : Lists
+			ul: {
+				paddingLeft: 40,
+				marginBottom: BASE_FONT_SIZE
+			},
+			ol: {
+				paddingLeft: 40,
+				marginBottom: BASE_FONT_SIZE
+			},
 
-            // Typography : Breaks
-            br: {
+			// Typography : Breaks
+			br: {
 
-            },
-            hr: {
-                marginTop: BASE_FONT_SIZE / 2,
-                marginBottom: BASE_FONT_SIZE / 2,
-                height: 1,
-                backgroundColor: '#CCC'
-            }
-        });
-    }
+			},
+			hr: {
+				marginTop: BASE_FONT_SIZE / 2,
+				marginBottom: BASE_FONT_SIZE / 2,
+				height: 1,
+				backgroundColor: '#CCC'
+			}
+		});
+	}
 
-  /* ****************************************************************************/
-  // Converting
-  /* ****************************************************************************/
+	/* ****************************************************************************/
+	// Converting
+	/* ****************************************************************************/
 
-  /**
-  * Converts a html style string to an object
-  * @param str: the style string
-  * @return the style as an obect
-  */
-    cssStringToObject (str) {
-        return str
-      .split(';')
-      .map((prop) => prop.split(':'))
-      .reduce((acc, prop) => {
-          if (prop.length === 2) {
-              acc[prop[0].trim()] = prop[1].trim();
-          }
-          return acc;
-      }, {});
-    }
+	/**
+	* Converts a html style string to an object
+	* @param str: the style string
+	* @return the style as an obect
+	*/
+	cssStringToObject(str) {
+		return str
+			.split(';')
+			.map((prop) => prop.split(':'))
+			.reduce((acc, prop) => {
+				if (prop.length === 2) {
+					acc[prop[0].trim()] = prop[1].trim();
+				}
+				return acc;
+			}, {});
+	}
 
-  /**
-  * Converts a html style to its equavalent react native style
-  * @param: css: object of key value css strings
-  * @param styleset: the styleset to convert the styles against
-  * @return an object of react native styles
-  */
-    cssToRNStyle (css, styleset) {
-        const styleProps = stylePropTypes[styleset];
-        return Object.keys(css)
-      .map((key) => [key, css[key]])
-      .map(([key, value]) => {
-        // Key convert
-          return [
-              key
-            .split('-')
-            .map((item, index) => index === 0 ? item : item[0].toUpperCase() + item.substr(1))
-            .join(''),
-              value];
-      })
-      .map(([key, value]) => {
-          if (!styleProps[key]) { return undefined; }
+	/**
+	* Converts a html style to its equavalent react native style
+	* @param: css: object of key value css strings
+	* @param styleset: the styleset to convert the styles against
+	* @return an object of react native styles
+	*/
+	cssToRNStyle(css, styleset) {
+		const styleProps = stylePropTypes[styleset];
+		return Object.keys(css)
+			.map((key) => [key, css[key]])
+			.map(([key, value]) => {
+				// Key convert
+				return [
+					key
+						.split('-')
+						.map((item, index) => index === 0 ? item : item[0].toUpperCase() + item.substr(1))
+						.join(''),
+					value];
+			})
+			.map(([key, value]) => {
+				if (!styleProps[key]) { return undefined; }
 
-          const testStyle = {};
-          testStyle[key] = value;
-          if (styleProps[key](testStyle, key, '')) {
-          // See if we can convert a 20px to a 20 automagically
-              if (styleProps[key] === React.PropTypes.number) {
-                  const numericValue = parseFloat(value.replace('px', ''));
-                  if (!isNaN(numericValue)) {
-                      testStyle[key] = numericValue;
-                      if (!styleProps[key](testStyle, key, '')) {
-                          return [key, numericValue];
-                      }
-                  }
-              }
-              return undefined;
-          }
-          return [key, value];
-      })
-      .filter((prop) => prop !== undefined)
-      .reduce((acc, [key, value]) => {
-          acc[key] = value;
-          return acc;
-      }, {});
-    }
+				const testStyle = {};
+				testStyle[key] = value;
+				if (styleProps[key](testStyle, key, '')) {
+					// See if we can convert a 20px to a 20 automagically
+					if (styleProps[key] === React.PropTypes.number) {
+						const numericValue = parseFloat(value.replace('px', ''));
+						if (!isNaN(numericValue)) {
+							testStyle[key] = numericValue;
+							if (!styleProps[key](testStyle, key, '')) {
+								return [key, numericValue];
+							}
+						}
+					}
+					return undefined;
+				}
+				return [key, value];
+			})
+			.filter((prop) => prop !== undefined)
+			.reduce((acc, [key, value]) => {
+				acc[key] = value;
+				return acc;
+			}, {});
+	}
 
-  /**
-  * @param str: the css style string
-  * @param styleset=STYLESETS.TEXT: the styleset to convert the styles against
-  * @return a react native style object
-  */
-    cssStringToRNStyle (str, styleset = STYLESETS.TEXT) {
-        return this.cssToRNStyle(this.cssStringToObject(str), styleset);
-    }
+	/**
+	* @param str: the css style string
+	* @param styleset=STYLESETS.TEXT: the styleset to convert the styles against
+	* @return a react native style object
+	*/
+	cssStringToRNStyle(str, styleset = STYLESETS.TEXT) {
+		return this.cssToRNStyle(this.cssStringToObject(str), styleset);
+	}
 }
 
 export default new HTMLStyles();
