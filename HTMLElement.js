d8214f81796bd34b084316985a4085178eb0afd4
Kangyun Wang <wky@Kangyuns-MacBook-Pro.local>
2017-08-11 11:20:32 +0200

    minor logic adjustment

--- HTMLElement.js
+++ HTMLElement.js
@@ -82,7 +82,7 @@ class HTMLElement extends React.Component {
 	render() {
 		const { htmlStyles, tagName, htmlAttribs, renderers, children, imagesMaxWidth, ...passProps } = this.props;
 
-		if (htmlAttribs.style && !htmlAttribs['data-attr']) {
+		if (htmlAttribs.style && !htmlAttribs['data-attr'] === 'include-styling') {
 			delete htmlAttribs.style;
 			delete htmlAttribs.class;
 		}
